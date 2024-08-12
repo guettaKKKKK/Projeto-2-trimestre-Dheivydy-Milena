ef6c48e2589f1ad62f6e490f40575fd4d16fb2ab
guettaKKKKK <143001598+guettaKKKKK@users.noreply.github.com>
2024-08-12 11:30:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,90 +6,108 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Que ideia vem a sua mente quando se vê o uso de computadores, celulares e tablets no ambiente escolar?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é muito bom! Está ajudando a expandir conhecimento e facilitando tarefas escolares.",
-                afirmacao: "afirmação",
-              },
-              {
-                texto: "Não é bom! Dispersa os estudantes e não expande o conhecimento."
-                afirmacao: "afirmação",
-              },
-            ],
-          },
-    {
-        enunciado: "Quais desafios você enfrenta ao incorporar tecnologias em suas aulas?",
-        alternativas: [
-            {
-                texto: "Curva de aprendizado das novas ferramentas e plataformas.",
-                afirmacao: "afirmação",
-              },
-              {
-                texto: "Dificuldade de acesso à internet estável e dispositivos suficientes para todos os alunos.",
-                afirmacao: "afirmação",
-            },
-          ],
-        },
-    {
-        enunciado: "Como você utiliza as tecnologias para promover a colaboração entre os alunos?",
-        alternativas: [
-            {
-                texto: "Através de plataformas online para trabalho em grupo e compartilhamento de documentos.",
-                afirmacao: "afirmação",
+                texto: "Isso é assustador!",
+                afirmacao: "afirmação"
             },
             {
-              texto: "Principalmente através de métodos tradicionais de interação em sala de aula."
-              afirmacao: "afirmação",
-            },
-          ],
-        },
-    {
-        enunciado: "Em sua opinião, as tecnologias ajudam a motivar os alunos no processo de aprendizagem?",
-        alternativas: [
-            {
-                texto: "Sim, as tecnologias oferecem recursos interativos e interessantes.",
-                afirmacao: "afirmação",
-            },
-            {
-              texto: "Não necessariamente, a motivação dos alunos depende mais da metodologia de ensino do que das tecnologias."
-              afirmacao: "afirmação",
-            },
+                texto: "Isso é maravilhoso!",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-        enunciado: "Como você vê o futuro da educação com o avanço contínuo das tecnologias?",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Promissor, as tecnologias têm o potencial de transformar profundamente a forma como aprendemos.",
-                afirmacao: "afirmação",
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "afirmação"
             },
             {
-              texto:"Incerto, há preocupações sobre como integrar as tecnologias de forma eficaz e equitativa na educação."
-              afirmacao: "afirmação",
-        },
-      ],
-    }
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
 ];
+
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function () {
-            atual++;
-            mostraPergunta();
-        })
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
 mostraPergunta();
